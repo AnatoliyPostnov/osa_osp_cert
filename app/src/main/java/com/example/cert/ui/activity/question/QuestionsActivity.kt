e1a949fc7f05eb58ba59b25d0e5789dcd168876f
apostnov <apostnov@telematika.com>
2024-03-20 17:52:58 +0300

    main activity

--- app/src/main/java/com/example/cert/ui/activity/question/QuestionsActivity.kt
+++ app/src/main/java/com/example/cert/ui/activity/question/QuestionsActivity.kt
@@ -480,24 +480,14 @@ fun Answers(answer: AnswerDomainDto, viewModel: QuestionActivityViewModel, quest
 
 
 val markdownContent5 = """ 
-    Given:
     ```
-    3. public class Redwood extends Tree {
-    4.   public static void main(String[] args) {
-    5.     new Redwood().go();
-    6.   }
-    7.   void go() {                                
-    8.     go2(new Tree(), new Redwood());          
-    9.     go2((Redwood) new Tree(), new Redwood());
-    10.  }
-    11.  void go2(Tree t1, Redwood r1) {
-    12.  Redwood r2 = (Redwood)t1;      
-    13.     Tree t2 = (Tree)r1;         
-    14.  }                              
-    15. }
-    16. class Tree { }
+    import java.util.*;
+        interface Jammer extends Quizel { }
+        interface Klakker { }
+        interface Quizel extends Klakker { } 
+        interface Floozel extends Jammer, Floozet { } 
+        interface Floozet { }
     ```
-    What is the result? (Choose all that apply.)
 """.trimIndent()
 
 
