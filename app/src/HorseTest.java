a93f8c58fe28005cd771402853e4fccfff000b7c
apostnov <apostnov@telematika.com>
2024-07-19 10:50:32 +0300

    main activity

---
+++ app/src/HorseTest.java
@@ -0,0 +1,18 @@
+Given:
+1. public class HorseTest {
+public static void main(String[] args) {
+  Object o = new Object() {
+public boolean equals(Object obj) {
+  return true;
+    2.
+    3.
+    4.
+    5.
+    6.
+    7.       }
+    8.     }
+    9.     Object obj = new Horse("Zippo");
+   10.     System.out.println(obj.name);
+11. }
+12. }
+What is the result?
